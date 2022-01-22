768234853161368c45e7bddc6fc9980597c4615a
Hetanshu Pandya <75762759+pandya2703@users.noreply.github.com>
2022-01-22 16:35:48 -0500

    Create image_reader.py

---
+++ image_reader.py
@@ -0,0 +1,8 @@
+def read_Image(file_path): 
+  pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" 
+  # Reading image 
+  img = cv2.imread(file_path) 
+  # Convert to RGB 
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+  # Detect texts from image 
+  texts = pytesseract.image_to_string(img) print(texts)
