277baf607dcee807fc25bbf416b70ccbb37f41f7
Alex Eckardt <realalexeck@gmail.com>
2022-01-22 16:29:22 -0500

    Added app.py skeleton structure

---
+++ app.py
@@ -0,0 +1,29 @@
+from loadjson import getProblemDictionary;
+#from speech import *;
+
+#Main Loop
+if (__name__ == "__main__"):
+
+    #
+    #Import
+    problemDict = getProblemDictionary();
+
+    #
+    #Get String from Speech
+
+
+    #
+    #Decide what type of function the string denotes
+
+
+    #
+    #Seperate the Speech Speach string into it's key variables based on the type of function
+
+
+    #
+    #Exectue the function using the variables
+
+
+    #
+    #Create a stringified version to send back to the person.
+    pass
