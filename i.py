4b82b3547c7b895fefda2a668814e790de6458cf
Madhav Bezawada <madhav.bezawada@hotmail.com>
2022-01-22 20:11:44 -0500

    made for loops more efficient

--- i.py
+++ i.py
@@ -3,7 +3,7 @@ def functionType(s):
     intrs = 0
     mn = 0
     words = s.split()
-    key = {("maximize","maximum","revenue"):"Maximum Revenue",("minimize","minimum","revenue"):"Minimum Revenue",("interest","rate", "principal"):"Interest"}
+    key = [("maximize","maximum","revenue","max"), ("minimize","minimum","revenue","min"), ("interest","rate", "principal")]
    
     for a in key[0]:
         if a in words:
@@ -23,32 +23,3 @@ def functionType(s):
         return "Minimum Revenue"
     elif max(li)== intrs:
         return "Interest"
-print(functionType("minimum the revenue if for every 1 dollar i add to the flowers price then 1 fewer people buy it. 25 people originally by the flowers and the flowers cost 10 dollars"))
-
-def maxRev(s):
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    for c in punctuations:
-        s = s.replace(c,"")
-    segements = []
-    word = s.split()
-    x = 0
-    while word != []:
-        l =[]
-        try:
-            int(word[x])
-            for y in range(x):
-                l.append(word[y])
-            segements.append((l,word[x]))
-            del(word[0:y+2])
-            x=0
-        except ValueError:
-            x=x+1
-            pass
-    segements 
-    return segements
-
-def minRev(s):
-    return 0
-
-def interest(s):
-    return 0
