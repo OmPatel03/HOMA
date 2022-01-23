27c5c6479ca737d09bdd7f0343b69bb77f4f193a
Om Patel <ompatel712003@gmail.com>
2022-01-22 21:17:12 -0500

    app changes

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-from loadjson import getProblemDictionary;
+from loadjson import getProblemDictionary
 #from speech import *;
 from image_reader import readImage
+import streamlit as st
 
-#Main Loop
+# Main Loop
 if (__name__ == "__main__"):
 
-    #
-    #Import
-    problemDict = getProblemDictionary();
+    st.title("HOMA")
+    user_input = st.text_input("Math Problem")
 
     #
-    #Get String from Speech or Image
+    # Import
+    problemDict = getProblemDictionary()
+
+    #
+    # Get String from Speech or Image
     file_path = "path of the file we want to read and extract the text from"
-    imageText = readImage(file_path)
+    #imageText = readImage(file_path)
 
     #
-    #Decide what type of function the string denotes
-
-
-    #
-    #Seperate the Speech Speach string into it's key variables based on the type of function
-
+    # Decide what type of function the string denotes
 
     #
-    #Exectue the function using the variables
-
+    # Seperate the Speech Speach string into it's key variables based on the type of function
 
     #
-    #Create a stringified version to send back to the person.
+    # Exectue the function using the variables
 
+    #
+    # Create a stringified version to send back to the person.
 
-    #end
+    # end
     pass
